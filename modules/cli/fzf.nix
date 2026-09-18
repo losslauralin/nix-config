845cee47e2527e413d958394eb635ae895d4020a
Loss <lossilklauralin@gmail.com>
2026-09-19 00:59:01 +0800

    fix: let atuin own Ctrl-R; disable the fzf history widget

--- modules/cli/fzf.nix
+++ modules/cli/fzf.nix
@@ -2,6 +2,8 @@
   lossilk.cli._.fzf.homeManager = {
     programs.fzf = {
       enable = true;
+      # atuin owns Ctrl-R (shell history); keep fzf for files/completion only.
+      historyWidget.command = "";
       defaultCommand = "fd --type f --hidden --follow --exclude .git";
       defaultOptions = [
         "--height 40%"
